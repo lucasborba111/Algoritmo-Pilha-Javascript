f3373087cc02a4db27c6ffc47a3c4ba66c2ebcc6
Lucas Borges Borba <69261686+lucasborba111@users.noreply.github.com>
2022-10-31 16:32:22 -0300

    Add files via upload

---
+++ algoritmo de pilha.js
@@ -0,0 +1,43 @@
+class Pilha {
+  constructor(capacidade) {
+    this.capacidade = capacidade;
+    this.topo = -1;
+    this.arr = [];
+  }
+  pilhaCheia() {
+    if (this.topo === this.capacidade - 1) {
+      return true;
+    }
+    return false;
+  }
+  pilhaVazia() {
+    if (this.topo === -1) {
+      return true;
+    }
+    return false;
+  }
+  inserirPilha(valor) {
+    if (!this.pilhaCheia()) {
+      this.arr.push(valor);
+      this.topo++;
+    } else {
+      console.log("Pilha cheia");
+    }
+  }
+  esvaziarPilha() {
+    if (!this.pilhaVazia()) {
+      this.arr.pop();
+      this.topo--;
+    } else {
+      console.log("Pilha vazia");
+    }
+  }
+  mostrarPilha() {
+    console.log(this.arr);
+  }
+}
+let pilha = new Pilha(5);
+pilha.inserirPilha(1);
+pilha.esvaziarPilha();
+pilha.esvaziarPilha();
+pilha.mostrarPilha();
